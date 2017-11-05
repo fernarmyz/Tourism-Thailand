2b1e22596fb2f7194c092103516fce70bac78695
Puttimate Thammasaeng <jusmistic@gmail.com>
2017-11-05 12:24:47 +0700

    refectir datacrawlers.py

--- dataset/datacrawlers.py
+++ dataset/datacrawlers.py
@@ -1,11 +1,16 @@
 """ data crawlers program use to crawlers dataset from tourism.go.th """
 import requests
 from bs4 import BeautifulSoup as bs
-def main():
-    """ main function """
-    web = requests.get("http://www.tourism.go.th/view/1/สถิตินักท่องเที่ยวชาวต่างชาติที่เดินทางเข้าประเทศไทย%20ปี%202559/TH-TH").text
+def find_link_intable(url):
+    """ Find link of dataset for download """
+    web = requests.get(url).text
     soup = bs(web, 'html.parser')
     file_table = soup.find('table', attrs={"class":"table-striped"}) #Find table that contain link of dataset
     for item in file_table.find_all('a'):
         print(item['href'])
+def main():
+    """ main function """
+    url = "http://www.tourism.go.th/view/1/สถิตินักท่องเที่ยวชาวต่างชาติที่เดินทางเข้าประเทศไทย%20ปี%202559/TH-TH"
+    find_link_intable(url)
+
 main()
