08bf99883e10bfbf0d52ee4c58d96ffb36d87e5b
Puttimate Thammasaeng <jusmistic@gmail.com>
2017-11-05 12:05:03 +0700

    Add BeatifulSoup in datacrawlers.py

--- dataset/datacrawlers.py
+++ dataset/datacrawlers.py
@@ -1,8 +1,9 @@
 """ data crawlers program use to crawlers dataset from tourism.go.th """
 import requests
-
+from bs4 import BeautifulSoup as bs
 def main():
     """ main function """
-    req = requests.get("http://www.tourism.go.th/view/1/สถิตินักท่องเที่ยวชาวต่างชาติที่เดินทางเข้าประเทศไทย%20ปี%202559/TH-TH")
-    print(req.status_code)
+    web = requests.get("http://www.tourism.go.th/view/1/สถิตินักท่องเที่ยวชาวต่างชาติที่เดินทางเข้าประเทศไทย%20ปี%202559/TH-TH").text
+    soup = bs(web, 'html.parser')
+    print(soup)
 main()
