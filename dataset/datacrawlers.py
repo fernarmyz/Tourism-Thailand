2fa4c78fcb30e6674fec52c870d6921a93c2c4a1
Puttimate Thammasaeng <jusmistic@gmail.com>
2017-11-05 12:29:01 +0700

    fix datacrawler return links list

--- dataset/datacrawlers.py
+++ dataset/datacrawlers.py
@@ -3,14 +3,16 @@ import requests
 from bs4 import BeautifulSoup as bs
 def find_link_intable(url):
     """ Find link of dataset for download """
+    links = []
     web = requests.get(url).text
     soup = bs(web, 'html.parser')
     file_table = soup.find('table', attrs={"class":"table-striped"}) #Find table that contain link of dataset
     for item in file_table.find_all('a'):
-        print(item['href'])
+        links.append(item['href'])
+    return links
 def main():
     """ main function """
     url = "http://www.tourism.go.th/view/1/สถิตินักท่องเที่ยวชาวต่างชาติที่เดินทางเข้าประเทศไทย%20ปี%202559/TH-TH"
-    find_link_intable(url)
-
+    links = find_link_intable(url)
+    print(links)
 main()
