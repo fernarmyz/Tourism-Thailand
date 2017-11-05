ae9c24fea9ff3edbd65ba75285ffbf73228ae529
Puttimate Thammasaeng <jusmistic@gmail.com>
2017-11-05 12:21:41 +0700

    add find link for download

--- dataset/datacrawlers.py
+++ dataset/datacrawlers.py
@@ -5,5 +5,7 @@ def main():
     """ main function """
     web = requests.get("http://www.tourism.go.th/view/1/สถิตินักท่องเที่ยวชาวต่างชาติที่เดินทางเข้าประเทศไทย%20ปี%202559/TH-TH").text
     soup = bs(web, 'html.parser')
-    print(soup)
+    file_table = soup.find('table', attrs={"class":"table-striped"}) #Find table that contain link of dataset
+    for item in file_table.find_all('a'):
+        print(item['href'])
 main()
