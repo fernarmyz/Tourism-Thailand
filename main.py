5fcb8113fd5921a4ea192e59acd1f3892874b741
Phongsathron Kittiworapanya <phongsathron@outlook.com>
2017-11-07 00:23:21 +0700

    Update title chart

--- main.py
+++ main.py
@@ -14,9 +14,8 @@ def plotgraph(filename, number):
     lst_year = dataframe.ix[0].index.values[1:].tolist()
     country = dataframe['Country']
 
-
     bar_chart = pygal.Line()
-    bar_chart.title = filename[8].upper() + filename[9:-13]
+    bar_chart.title = filename[8:-13].title()
     bar_chart.x_labels = map(str, lst_year)
     for i in range(len(dataframe)):
         bar_chart.add(country[i], dataframe.ix[i][1:])
