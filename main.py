7c2d6c292fd9b17bca34e0d6bfc65ce352758418
Dump Ma <poppular105@hotmail.com>
2017-11-06 19:39:08 +0700

    keep list of data 2550 - 2559

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ from pandas import ExcelWriter
 from pandas import ExcelFile
 import pygal
 
-def plotgraph(filename):
+def plotgraph(filename, number):
     """
         plot graph
     """
@@ -18,12 +18,11 @@ def plotgraph(filename):
     bar_chart = pygal.Line()
     bar_chart.title = filename[8].upper() + filename[9:-13]
     bar_chart.x_labels = map(str, lst_year)
-
     for i in range(len(dataframe)):
         bar_chart.add(country[i], dataframe.ix[i][1:])
 
     bar_chart.render_to_file("chart/"+filename[7:-5]+".svg")
-    return [filename[8:filename.find("_")],dataframe[2550]]
+    return [filename[8:filename.find("_")],dataframe[2550 + number]]
 
 def main():
     """
@@ -31,8 +30,19 @@ def main():
     """
     files = [filename for filename in open("dataset/file.txt")]
     list_x = []
-    for item in files:
-        list_x.append(plotgraph("dataset/" + item.strip("\n")))
+    list_x2 = []
+    list_sum = []
+    number = -1
+    lst = []
+    for item in (files * 7):
+        number += 1
+        if number > 9:
+            number -= ((number // 7) * 7)
+        list_x.append(plotgraph("dataset/" + item.strip("\n"), number))
     for i in list_x:
-         print(sum(i[1]))
+        lst.append(sum(i[1]))
+        if len(lst) == 7:
+            list_x2.append(lst)
+            lst = []
+    print(list_x2)
 main()
