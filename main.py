38284e3d34cb91a98fc2ef976cb7b3bf4a27eda6
Dump Ma <poppular105@hotmail.com>
2017-11-06 21:14:13 +0700

    change file to keep list_of_numberpeople

--- main.py
+++ main.py
@@ -22,7 +22,9 @@ def plotgraph(filename, number):
         bar_chart.add(country[i], dataframe.ix[i][1:])
 
     bar_chart.render_to_file("chart/"+filename[7:-5]+".svg")
-    return [filename[8:filename.find("_")],dataframe[2550 + number]]
+    if number + 2549 >= 2560:  # ถ้าเกิน 2560 ให้ตัดเหลือ 2559 เพราะมันมีถึงปี 2559
+        number -= 1
+    return [filename[8:filename.find("_")],dataframe[2549 + number]]
 
 def main():
     """
@@ -30,19 +32,28 @@ def main():
     """
     files = [filename for filename in open("dataset/file.txt")]
     list_x = []
-    list_x2 = []
+    list_get_lst_of_sum = []  #เก็บlistผลรวมจำนวนคนในแต่ละทวีป โดยในlist จะมี list ย่อย 10 list คือ ปี 2550 - 2559 และในแต่ละ list ย่อยมี 7 index คือทวีป
     list_sum = []
-    number = -1
-    lst = []
-    for item in (files * 7):
-        number += 1
-        if number > 9:
-            number -= ((number // 7) * 7)
-        list_x.append(plotgraph("dataset/" + item.strip("\n"), number))
+    list_of_number_and_lanmass = []
+    list_country = ['africa', 'america', 'east asia', 'europe', 'middle east', 'oceania', 'south asia']
+    number_to_plotgraph_function_for_add_years = 1  # send to plotgraph function to +1 year
+    count = 0 # count if count == 7 it means 7 ทวีป  change year example: year 2554 and count % 7 == 0  >>> year += 1 เริ่มต้นทวีปใหม่
+    lst = [] #list for keep sum(i[1]) กคือผลรวมจำนวนคน
+    for item in (files * 10):  # * 10 for 10 years
+        count += 1
+        list_x.append(plotgraph("dataset/" + item.strip("\n"), number_to_plotgraph_function_for_add_years))
+        if count % 7 == 0:  # count คือพอเป็น 7 ทวีปละให้มันเปลี่ยนปี (year += 1)
+            number_to_plotgraph_function_for_add_years += 1
     for i in list_x:
-        lst.append(sum(i[1]))
-        if len(lst) == 7:
-            list_x2.append(lst)
+        lst.append(sum(i[1])) #keep sum of people number
+        if len(lst) == 7: #its means เก็บครบ 7 ทวีปเหมือนเดิม append lst to list_get_lst_of_sum
+            list_get_lst_of_sum.append(lst)
             lst = []
-    print(list_x2)
+    for i in range(7): #7 ทวีป
+        for j in range(10): # 10 years
+            lst.append(list_get_lst_of_sum[j][i]) #เก็บ list ใหญ่ index j  และ list เล็ก index i เพื่อแยกเปนทวีปๆ
+        lst.append(list_country[i])
+        list_of_number_and_lanmass.append(lst) #add name country
+        lst = []
+    print(list_of_number_and_lanmass)
 main()
