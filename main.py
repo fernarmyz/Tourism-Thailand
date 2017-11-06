e11467668fa2dcf95162a25b30033c3a51d89552
Puttimate "Jusmistic" Thammasang <jusmistic@gmail.com>
2017-11-06 16:07:44 +0700

    add some per year

--- main.py
+++ main.py
@@ -6,16 +6,15 @@ from pandas import ExcelWriter
 from pandas import ExcelFile
 import pygal
 
-#use data by file to plot graph by using data of landmass.
-#this is a data of number people who tourism in thailand Classified by country.
-#get Number of people to plot graph.
 def plotgraph(filename):
-    """plot graph"""
+    """
+        plot graph
+    """
     dataframe = pd.read_excel(filename)
     lst_year = dataframe.ix[0].index.values[1:].tolist()
     country = dataframe['Country']
 
-    print(dataframe[2550])
+
     bar_chart = pygal.Line()
     bar_chart.title = filename[8].upper() + filename[9:-13]
     bar_chart.x_labels = map(str, lst_year)
@@ -24,11 +23,16 @@ def plotgraph(filename):
         bar_chart.add(country[i], dataframe.ix[i][1:])
 
     bar_chart.render_to_file("chart/"+filename[7:-5]+".svg")
+    return [filename[8:filename.find("_")],dataframe[2550]]
 
 def main():
-    """main function"""
+    """
+        main function
+    """
     files = [filename for filename in open("dataset/file.txt")]
+    list_x = []
     for item in files:
-        plotgraph("dataset/" + item.strip("\n"))
-
+        list_x.append(plotgraph("dataset/" + item.strip("\n")))
+    for i in list_x:
+         print(sum(i[1]))
 main()
